fa216bf250609de986d0950e29142d24aa5c3d73
y617101 <48057966+y617101@users.noreply.github.com>
2026-02-22 18:31:21 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,9 +39,6 @@ def test_write_one_row():
 # ================================
 # Token Symbol Map (Base)
 ADDRESS_SYMBOL_MAP = {
-
-# Token Symbol Map (Base)
-ADDRESS_SYMBOL_MAP = {
     "0x4200000000000000000000000000000000000006": "WETH",
     "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913": "USDC",
 }
